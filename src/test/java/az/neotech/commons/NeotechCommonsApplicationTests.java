5745302bca41328f0a2a4722869eec5104e893f7
Mammad Yahyayev <mammadyahyayev@gmail.com>
2025-05-15 11:14:15 +0400

    initial commit

---
+++ src/test/java/az/neotech/commons/NeotechCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.neotech.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeotechCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
