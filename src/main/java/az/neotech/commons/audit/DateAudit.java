1019454277e46a93a42617e5aa24bf73f8d66445
Mammad Yahyayev <mammadyahyayev@gmail.com>
2025-05-15 14:08:55 +0400

    add audit classes

---
+++ src/main/java/az/neotech/commons/audit/DateAudit.java
@@ -0,0 +1,9 @@
+package az.neotech.commons.audit;
+
+import java.time.LocalDateTime;
+
+public class DateAudit {
+
+    protected LocalDateTime createdDateTime;
+    protected LocalDateTime modifiedDateTime;
+}
---
+++ src/main/java/az/neotech/commons/audit/DetailedAudit.java
@@ -0,0 +1,10 @@
+package az.neotech.commons.audit;
+
+import java.time.LocalDateTime;
+
+public class DetailedAudit {
+    protected String createdBy;
+    protected String modifiedBy;
+    protected LocalDateTime createdDateTime;
+    protected LocalDateTime modifiedDateTime;
+}
